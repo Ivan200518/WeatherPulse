958f7b3fe6b37b2b9876595f337df2b424d3d751
ivan200518 <ivan.ivancleh@gmail.com>
2025-01-05 22:02:24 +0000

    Change Color

--- app/src/main/java/com/example/weatherpulse/ui/theme/Color.kt
+++ app/src/main/java/com/example/weatherpulse/ui/theme/Color.kt
@@ -8,5 +8,5 @@ val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
-val BlueLight = Color(0x7E1594CD)
+val BlueLight = Color(0x7E0F9DDC)
 val White = Color(0x7E10C6C6)
